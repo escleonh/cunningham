970dfdceb3defaa60a5ae1b4eeece6f7bf9e9a7e
escleonh <hescalante@atu.edu>
2021-07-17 01:10:36 -0500

    project upload

---
+++ src/Nav-bar.js
@@ -0,0 +1,20 @@
+import './Nav-bar.css';
+const Navbar = () => {
+    return <nav>
+    <div>LOGO</div>
+    <div>
+    <ul>
+        
+        <li><a href="">Services</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Contact Us</a></li>
+    </ul>
+    </div>
+    <div>
+        <a href="#">Free Quote</a>
+    </div>
+
+    </nav>;
+}
+ 
+export default Navbar;
---
+++ src/Top-bar.js
@@ -0,0 +1,11 @@
+import './Top-bar.css';
+const Topbar = () => {
+    return <div className="topBar">
+        <ul>
+            <li><a href="tel:4793551488">(479) 355 1488</a></li>
+            <li><a href="mailto:hescalante@atu.edu">Hescalante@atu.edu</a></li>
+        </ul>
+    </div>;
+}
+ 
+export default Topbar;
