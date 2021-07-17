d44b352276c82ad54636e971c2fee543dd1d345e
escleonh <hescalante@atu.edu>
2021-07-17 02:25:33 -0500

    nav area now responsive

--- src/Nav-bar.js
+++ src/Nav-bar.js
@@ -1,17 +1,31 @@
 import './Nav-bar.css';
 const Navbar = () => {
     return <nav>
+    <div className='mobileLeftNavCol'></div>
     <div>LOGO</div>
-    <div>
-    <ul>
-        
+    <div className='mainMenu'>
+    <ul >
         <li><a href="">Services</a></li>
         <li><a href="">About Us</a></li>
         <li><a href="">Contact Us</a></li>
     </ul>
     </div>
-    <div>
+    <div className='quoteBtn'>
         <a href="#">Free Quote</a>
+    </div>
+
+    <div className="menuIcon">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+
+    <div className='slidebarmenu'>
+    <ul>
+        <li><a href="">Services</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Contact Us</a></li>
+    </ul>
     </div>
 
     </nav>;
