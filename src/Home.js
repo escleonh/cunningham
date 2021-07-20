0b7b42577d0ff6b0e4780693398acbd426f57cc8
escleonh <hescalante@atu.edu>
2021-07-20 00:26:00 -0500

    services section additions

--- src/Home.js
+++ src/Home.js
@@ -14,61 +14,56 @@ const Home = () => {
                         </div>
                     </div>
                 </div> 
-
+                <div className="titleHeading"><h1>Services</h1></div>
                 <div className="servicesContiner">
                     <div>
                         <div className="serviceOneImg">
-                            Image goes here.
                         </div>
                         <div className="serviceTitle">
-                            Service title goes here.
+                           <h2>Service Title</h2>
                         </div>
                         <div className="serviceDescription">
-                            Service description goes here.
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                         </div>
                     </div>
                     <div>
                         <div className="serviceOneImg">
-                            Image goes here.
                         </div>
                         <div className="serviceTitle">
-                            Service title goes here.
+                        <h2>Service Title</h2>
                         </div>
                         <div className="serviceDescription">
-                            Service description goes here.
+                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                         </div>
                     </div>
                     <div>
                         <div className="serviceOneImg">
-                            Image goes here.
                         </div>
                         <div className="serviceTitle">
-                            Service title goes here.
+                        <h2>Service Title</h2>
                         </div>
                         <div className="serviceDescription">
-                            Service description goes here.
+                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                         </div>
                     </div>
                     <div>
                         <div className="serviceOneImg">
-                            Image goes here.
                         </div>
                         <div className="serviceTitle">
-                            Service title goes here.
+                        <h2>Service Title</h2>
                         </div>
                         <div className="serviceDescription">
-                            Service description goes here.
+                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                         </div>
                     </div>
                     <div>
                         <div className="serviceOneImg">
-                            Image goes here.
                         </div>
                         <div className="serviceTitle">
-                            Service title goes here.
+                        <h2>Service Title</h2>
                         </div>
                         <div className="serviceDescription">
-                            Service description goes here.
+                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                         </div>
                     </div>
                     
